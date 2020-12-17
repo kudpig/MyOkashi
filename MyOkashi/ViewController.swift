bfb60db235e8b8bd343006d6473db43fdedb7a1c
kudpig <shin.kudo.aa@gmail.com>
2020-12-18 04:37:18 +0900

    [add]UIオブジェクトの関連付け

--- MyOkashi/ViewController.swift
+++ MyOkashi/ViewController.swift
@@ -15,6 +15,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBOutlet weak var searchText: UISearchBar!
+    
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
 }
 
